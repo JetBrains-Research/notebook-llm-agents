d228dc7cd189922bb64f2b13fc20a461aac75c13
Konstantin.Grotov <konstantin.grotov@gmail.com>
2024-02-13 11:28:21 +0100

    add function calling example and simulator of notebook actions

--- src/preprocessing/notebook.py
+++ src/preprocessing/notebook.py
@@ -1,7 +1,11 @@
+import io
+import sys
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional, Self
+
+from tqdm import tqdm
 
 from src import ROOT_PATH
 from src.preprocessing.process_notebook import notebook_to_string
@@ -32,6 +36,9 @@ class NotebookBase(ABC):
 
 
 class StringNotebook(NotebookBase):
+    def execute_cell(self, cell_num: int) -> Cell:
+        pass
+
     def __init__(self, notebook_path: Path, sep: str = "\n#%% --\n"):
         self.notebook_path = notebook_path
         self.sep = sep
@@ -46,8 +53,52 @@ class StringNotebook(NotebookBase):
         ]
         return cells
 
-    def execute_cell(self, cell_num: int) -> Cell:
-        pass
+    def _prepare_for_execution(self, cells: list[Cell]):
+        source = self.sep.join([f"{cell.cell_source}" for cell in cells])
+        return source
+
+    def execute_cells(self, cells: list[Cell]) -> Cell:
+        code = self._prepare_for_execution(cells)
+        print(code)
+        print("." * 20)
+        stdout, stderr = io.StringIO(), io.StringIO()
+
+        prev_stdout, prev_stderr = sys.stdout, sys.stderr
+        sys.stdout, sys.stderr = stdout, stderr
+
+        cell_output = {"text": None, "error": None}
+        try:
+            exec(code)
+        except:
+            cell_output["error"] = True
+        finally:
+            cell = cells[-1]
+            cell_output["text"] = stdout.getvalue()
+            cell.cell_output = cell_output
+
+            sys.stdout, sys.stderr = prev_stdout, prev_stderr
+            stdout.close()
+            stderr.close()
+
+            return cell
+
+    def execute_all(
+        self, progress_bar: bool = True, exclude_nums: Optional[list[int]] = None
+    ) -> tuple[bool, Optional[int]]:
+        chosen_cells = (
+            self.cells
+            if exclude_nums is None
+            else [obj for i, obj in enumerate(self.cells) if i not in exclude_nums]
+        )
+
+        gen = tqdm(enumerate(chosen_cells)) if progress_bar else enumerate(chosen_cells)
+        for num, _ in gen:
+            resulted_cell = self.execute_cells(chosen_cells[: num + 1])
+
+            if resulted_cell.cell_output.get("error") is not None:
+                print(f"Cell with num {num} caused an error")
+                return False, num
+        return True, None
 
     def add_cell(self, cell_source: str) -> Self:
         new_cell = Cell(cell_num=len(self.cells), cell_source=cell_source)
@@ -71,14 +122,13 @@ class StringNotebook(NotebookBase):
 
 
 if __name__ == "__main__":
-    ntb_path = Path(ROOT_PATH / "data/test_notebooks/test_notebook.ipynb")
+    ntb_path = Path(ROOT_PATH / "data/test_notebooks/test_notebook_2.ipynb")
     test_string, test_end = "print('hello world!')", f"\n{'='*10}\n"
 
     ntb = StringNotebook(ntb_path)
     print(ntb, end=test_end)
 
-    ntb = ntb.add_cell(test_string)
-    print(ntb, end=test_end)
-
-    ntb = ntb.change_cell(0, test_string)
-    print(ntb, end=test_end)
+    res, n = ntb.execute_all()
+    print(res, n)
+    res, n = ntb.execute_all(exclude_nums=(n,))
+    print(res, n)
